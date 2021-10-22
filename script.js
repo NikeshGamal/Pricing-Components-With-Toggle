f7e014c514ee8b40ed2390da867eddbbd781a389
Nikesh Gamal <nikesh.191624@ncit.edu.np>
2021-10-22 11:39:53 +0545

    Toggle Element Added

---
+++ script.js
@@ -0,0 +1,9 @@
+const togg= document.querySelector('.inner_container');
+
+// function nextPlan(){
+//       togg.classList.toggle("active");
+// }
+
+togg.addEventListener('click',(e)=>{
+    togg.classList.toggle("active");
+});

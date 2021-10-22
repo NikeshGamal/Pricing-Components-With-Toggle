1b3997d0fa34bc1883afdf72aae3bb436f68032c
Nikesh Gamal <nikesh.191624@ncit.edu.np>
2021-10-22 12:49:16 +0545

    Toggle Section Completed

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const togg= document.querySelector('.inner_container');
-
+const values=document.querySelectorAll('.plan_amt');
 // function nextPlan(){
 //       togg.classList.toggle("active");
 // }
-
+const monthlyCost=['$19.99','$24.99','$39.99'];
+const annualCost=['$199.99','$249.99','$399.99']
+ 
 togg.addEventListener('click',(e)=>{
     togg.classList.toggle("active");
+
+     for(let i=0;i < values.length;i++){
+         if(togg.classList.contains('active')){
+            values[i].innerHTML=monthlyCost[i];
+         }else{
+            values[i].innerHTML=annualCost[i];
+         }
+     }
+    
 });
